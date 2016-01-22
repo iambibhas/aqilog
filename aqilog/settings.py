ec97b78d7ac52431847fbd774fa551b1dd00edf5
Bibhas <iambibhas@gmail.com>
2016-01-23 02:43:05 +0530

    added local settingsW

--- aqilog/settings.py
+++ aqilog/settings.py
@@ -110,3 +110,8 @@ STATICFILES_DIRS = (
 )
 
 STATIC_ROOT = os.path.join(BASE_DIR, "assets", "collected")
+
+try:
+    from local_settings import *
+except Exception, e:
+    print('local settings not found')
